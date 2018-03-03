1367922699cbcf5803330d9127c03c8cec036f22
Ii Masamitsu <arcana278@gmail.com>
2018-03-03 16:31:36 +0900

    refactor: weather.adjustを整理した

--- core/weather.py
+++ core/weather.py
@@ -2,39 +2,65 @@ import core.type as t
 
 
 def adjust(weather, move):
-    if weather in ['Sunny', 'Drought']:
-        if move.type == t.Fire:
-            move.power *= 1.5
-        if move.type == t.Water:
-            if weather == 'Sunny':
-                move.power *= 0.5
-            if weather == 'Drought':
-                move.power = 0
-        if move.name == 'Weather Ball':
-            move.power *= 2.0
-            move.type = t.Fire
-    if weather in ['Rain', 'Heavy Rain']:
-        if move.type == t.Fire:
-            if weather == 'Rain':
-                move.power *= 0.5
-            if weather == 'Heavy Rain':
-                move.power = 0
-        if move.type == t.Water:
-            move.power *= 1.5
-        if move.name == 'Solar Beam':
-            move.power *= 0.5
-        if move.name == 'Weather Ball':
-            move.power *= 2.0
-            move.type = t.Water
+    if weather == 'Sunny':
+        sunny_adjust(move)
+    if weather == 'Drought':
+        drought_adjust(move)
+    if weather == 'Rain':
+        rainy_adjust(move)
+    if weather == 'Heavy Rain':
+        heavy_rainy_adjust(move)
     if weather == 'Hail':
-        if move.name == 'Solar Beam':
-            move.power *= 0.5
-        if move.name == 'Weather Ball':
-            move.power *= 2.0
-            move.type = t.Ice
+        hailstorm_adjust(move)
     if weather == 'Sandstorm':
-        if move.name == 'Solar Beam':
-            move.power *= 0.5
-        if move.name == 'Weather Ball':
-            move.power *= 2.0
-            move.type = t.Rock
+        sandstorm_adjust(move)
+
+
+def sunny_adjust(move):
+    if move.type == t.Fire:
+        move.power *= 1.5
+    if move.type == t.Water:
+        move.power *= 0.5
+    if move.name == 'Weather Ball':
+        move.power *= 2.0
+        move.type = t.Fire
+
+
+def drought_adjust(move):
+    sunny_adjust(move)
+    if move.type == t.Water:
+        move.power = 0
+
+
+def rainy_adjust(move):
+    if move.type == t.Fire:
+        move.power *= 0.5
+    if move.type == t.Water:
+        move.power *= 1.5
+    if move.name == 'Solar Beam':
+        move.power *= 0.5
+    if move.name == 'Weather Ball':
+        move.power *= 2.0
+        move.type = t.Water
+
+
+def heavy_rainy_adjust(move):
+    rainy_adjust(move)
+    if move.type == t.Fire:
+        move.power = 0
+
+
+def hailstorm_adjust(move):
+    if move.name == 'Solar Beam':
+        move.power *= 0.5
+    if move.name == 'Weather Ball':
+        move.power *= 2.0
+        move.type = t.Ice
+
+
+def sandstorm_adjust(move):
+    if move.name == 'Solar Beam':
+        move.power *= 0.5
+    if move.name == 'Weather Ball':
+        move.power *= 2.0
+        move.type = t.Rock
