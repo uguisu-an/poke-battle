fd3c974d510599b0e3025546d697b31ad46d2b51
Ii Masamitsu <arcana278@gmail.com>
2018-03-09 08:24:13 +0900

    feat: FlowerGiftを追加した

--- core/stat_builder.py
+++ core/stat_builder.py
@@ -2,6 +2,7 @@ from copy import copy
 from core.monster import MonsterStat
 from core.character import *
 from core.item import *
+from core.effect.weather import *
 
 
 class StatBuilder:
@@ -11,6 +12,7 @@ class StatBuilder:
     _item = None
     _partner = None
     _partner_character = None
+    _weather = None
 
     def add_character(self, character):
         self._monster_character = character
@@ -18,11 +20,14 @@ class StatBuilder:
     def add_item(self, item):
         self._item = item
 
+    def add_weather(self, weather):
+        self._weather = weather
+
     def add_monster(self, monster):
         self._monster = monster
         self._monster_name = monster.name
-        self._monster_character = monster.character
-        self._item = monster.item
+        self.add_character(monster.character)
+        self.add_item(monster.item)
 
     def add_partner(self, partner):
         self._partner = partner
@@ -39,4 +44,6 @@ class StatBuilder:
             stat.sp_atk *= 1.5
         if self._item == ThickClub and self._monster_name in ('Cubone', 'Marowak'):
             stat.py_atk *= 2.0
+        if self._monster_character == FlowerGift and self._weather == Sunny:
+            stat.py_atk *= 1.5
         return stat
