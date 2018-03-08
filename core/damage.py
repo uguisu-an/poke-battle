76f90a4a0a21b2b70a1b073f469ccb36a327b880
Ii Masamitsu <arcana278@gmail.com>
2018-03-09 08:44:28 +0900

    feat: DamageCalculatorを作った

--- core/damage.py
+++ core/damage.py
@@ -1,4 +1,6 @@
 import random
+from core.move import Attack
+from core.monster import Monster
 
 
 # attack-moveに計算させてもいい
@@ -21,3 +23,28 @@ def maximizer(_, maximum):
 # 乱数を与える
 def randomized(damage, randomizer=default_randomizer):
     return damage * (randomizer(85, 100) / 100)
+
+
+class DamageCalculator:
+    randomizer = default_randomizer
+
+    def __init__(self):
+        self.move: Attack = None
+        self.attacker: Monster = None
+        self.defender: Monster = None
+
+    def add_randomizer(self, randomizer):
+        self.randomizer = randomizer
+
+    def calc_base_damage(self):
+        return base_damage(self.move.power, self.calc_attack_defence_ratio())
+
+    def calc_attack_defence_ratio(self):
+        return self.attacker.attack(self.move) / self.defender.defend(self.move)
+
+    def calc(self):
+        damage = self.calc_base_damage()
+        damage = randomized(damage, randomizer=self.randomizer)
+        damage = damage * self.move.type_match_bonus(self.attacker.type)
+        damage = damage * self.move.type_match_up(self.defender.type)
+        return damage
