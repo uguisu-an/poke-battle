87b0071e00e7573609fb3ff88830ea69018feb37
Ii Masamitsu <arcana278@gmail.com>
2018-03-09 09:02:14 +0900

    feat: アイテムと特性による補正を追加した

--- core/damage.py
+++ core/damage.py
@@ -1,6 +1,8 @@
 import random
 from core.move import Attack
 from core.monster import Monster
+from core.item import *
+from core.character import *
 
 
 # attack-moveに計算させてもいい
@@ -27,14 +29,42 @@ def randomized(damage, randomizer=default_randomizer):
 
 class DamageCalculator:
     randomizer = default_randomizer
+    attacker = None
+    attacker_item = None
+    attacker_character = None
+    defender = None
+    defender_item = None
+    defender_character = None
 
-    def __init__(self):
-        self.move: Attack = None
-        self.attacker: Monster = None
-        self.defender: Monster = None
+    def __init__(self, move, attacker, defender):
+        self.move: Attack = move
+        self.add_attacker(attacker)
+        self.add_defender(defender)
+
+    def add_attacker(self, monster):
+        self.attacker = monster
+        self.add_attacker_character(monster.character)
+        self.add_attacker_item(monster.item)
+
+    def add_defender(self, monster):
+        self.defender = monster
+        self.defender_character(monster.character)
+        self.defender_item(monster.item)
 
     def add_randomizer(self, randomizer):
         self.randomizer = randomizer
+
+    def add_attacker_item(self, item):
+        self.attacker_item = item
+
+    def add_defender_item(self, item):
+        self.defender_item = item
+
+    def add_attacker_character(self, character):
+        self.attacker_character = character
+
+    def add_defender_character(self, character):
+        self.defender_character = character
 
     def calc_base_damage(self):
         return base_damage(self.move.power, self.calc_attack_defence_ratio())
@@ -42,9 +72,21 @@ class DamageCalculator:
     def calc_attack_defence_ratio(self):
         return self.attacker.attack(self.move) / self.defender.defend(self.move)
 
+    def calc_item_bonus(self):
+        if self.defender_item == Berry:
+            return 0.5
+        return 1.0
+
+    def calc_character_bonus(self):
+        if self.attacker_character == BrainForce and self.move.type_match_up(self.defender.type) > 1:
+            return 1.2
+        return 1.0
+
     def calc(self):
         damage = self.calc_base_damage()
         damage = randomized(damage, randomizer=self.randomizer)
         damage = damage * self.move.type_match_bonus(self.attacker.type)
         damage = damage * self.move.type_match_up(self.defender.type)
+        damage = damage * self.calc_item_bonus()
+        damage = damage * self.calc_character_bonus()
         return damage
--- core/item.py
+++ core/item.py
@@ -3,3 +3,4 @@ from collections import namedtuple
 
 Item = namedtuple('Item', 'name')
 ThickClub = Item('Thick Club')
+Berry = Item('Berry')
