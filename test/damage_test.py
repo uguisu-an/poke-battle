4ab205247780f821b18dcaa0fd38f41d68a8acfc
Ii Masamitsu <arcana278@gmail.com>
2018-03-10 20:46:41 +0900

    test: powerとlevelはsetupで初期化することにした

--- test/damage_test.py
+++ test/damage_test.py
@@ -4,10 +4,13 @@ import core.damage2 as d2
 
 def setup():
     reload(d2)
+    d2.mv_power = 120
+    d2.mv_level = 50
 
 
 def test_base_damage():
-    d2.mv_power = 120
+    d2.at_stat = 100
+    d2.df_stat = 100
     assert d2.calc() == 54
     d2.at_stat = 150
     d2.df_stat = 50
@@ -17,14 +20,12 @@ def test_base_damage():
 
 
 def test_type_match():
-    d2.mv_power = 120
     d2.mv_type = d2.Type.Normal
     d2.at_type = {d2.Type.Fire, d2.Type.Normal}
     assert d2.calc() == 65
 
 
 def test_type_effect():
-    d2.mv_power = 120
     d2.mv_type = d2.Type.Dragon
     d2.df_type = {d2.Type.Dragon, d2.Type.Fairy}
     assert d2.calc() == 0
